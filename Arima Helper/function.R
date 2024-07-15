eca3aca27da9ffdc3ff59c6a7923379bb4944474
Isaac D. Haikal Azziz <69369431+Isaacdha@users.noreply.github.com>
2024-07-15 19:22:11 +0800

    Update function.R

--- Arima Helper/function.R
+++ Arima Helper/function.R
@@ -8,21 +8,26 @@ library(dplyr)
 #--------------------------------------------------------------------------------------------------------------------------------------
 
 # Transformation Function
-stationarity_test_transform <- function(ts.data) {
+stationarity_test_transform <- function(ts.data, scale = 1) {
+  # Apply scaling
+  ts.data <- ts.data * scale
+  
   # Variance Stationarity
-  lambda <- BoxCox.lambda(ts.data)
-  lambda_list <- c(lambda)
+  lambda_list <- numeric()
   trans.data <- ts.data
   
-  while (abs(tail(lambda_list, n = 1) - 1) > 0.1) {
-    trans.data <- BoxCox(trans.data, tail(lambda_list, n = 1))
-    lambda_list <- append(lambda_list, BoxCox.lambda(trans.data))
+  repeat {
+    lambda <- BoxCox.lambda(trans.data)
+    trans.data <- BoxCox(trans.data, lambda)
+    lambda_list <- c(lambda_list, lambda)
+    
+    if (abs(lambda - 1) <= 0.1) break
   }
   
   # Mean Stationarity
   n_diff <- 0
   diff.data <- trans.data 
-  adf_pval <- c(adf.test(diff.data)$p.value)
+  adf_pval <- adf.test(diff.data)$p.value
   
   while (tail(adf_pval, n = 1) > 0.05){
     diff.data <- diff(diff.data)
@@ -30,10 +35,24 @@ stationarity_test_transform <- function(ts.data) {
     adf_pval <- append(adf_pval, adf.test(diff.data)$p.value)
   }
   
-  listed <- list("trans.data" = trans.data, "lambda" = lambda_list, 
-                 "mean.trans.data" = diff.data, "adf.pvalue" = adf_pval, 
-                 "d" = n_diff)
-  comment(listed) <- "Basic Variance and Mean Stationarity Transformation. Please use Var Stationarity Data in next step. Mean Stationarity only for search (d) order"
+  # Create a list to store lambdas and scale
+  lambda_scale_list <- list(
+    lambdas = lambda_list,
+    scale = scale
+  )
+  
+  listed <- list(
+    trans.data = trans.data,
+    lambda_scale_list = lambda_scale_list, 
+    mean.trans.data = diff.data, 
+    adf.pvalue = adf_pval, 
+    d = n_diff
+  )
+  
+  comment(listed) <- "Basic Variance and Mean Stationarity Transformation. 
+  Please use Var Stationarity Data in next step. 
+  Mean Stationarity only for search (d) order"
+  
   return(listed)
 }
 
@@ -43,17 +62,14 @@ stationarity_test_transform <- function(ts.data) {
 generate_combinations <- function(p, d, q, P = NULL, D = NULL, Q = NULL) {
   if (is.null(P) || is.null(D) || is.null(Q)) {
     combinations <- expand.grid(p, d, q)
-    combinations <- combinations[!(combinations[, 1] == 0 
-                                   & combinations[, 3] == 0), ]
+    combinations <- combinations[!(combinations[, 1] == 0 & combinations[, 3] == 0), ]
     row.names(combinations) <- NULL
     colnames(combinations) <- c("Order p", "Order d", "Order q")
   } else {
     combinations <- expand.grid(p, d, q, P, D, Q)
-    combinations <- combinations[!(combinations[, 1] == 0 
-                                   & combinations[, 3] == 0), ]
+    combinations <- combinations[!(combinations[, 1] == 0 & combinations[, 3] == 0), ]
     row.names(combinations) <- NULL
-    colnames(combinations) <- c("Order p", "Order d", "Order q", "Order P", 
-                                "Order D", "Order Q")
+    colnames(combinations) <- c("Order p", "Order d", "Order q", "Order P", "Order D", "Order Q")
   }
   return(as.matrix(combinations))
 }
@@ -143,17 +159,14 @@ order_detector <- function(data, lag, s = NULL){
 }
 
 # Tentative Model List & Check
-manual_arima <- function(data, p, d, q, s = NULL, 
-                         P = NULL, D = NULL, Q = NULL, 
-                         xreg = NULL) {
-  
+# Tentative Model List & Check
+manual_arima <- function(data, p, d, q, s = NULL, P = NULL, D = NULL, Q = NULL, xreg = NULL) {
   # Parameter Combination
   parameter_combinations <- generate_combinations(p, d, q, P, D, Q)
   
-  # Check if its intervention
-  if (!is.null(xreg)){
-    parameter_combinations <- rbind(parameter_combinations, 
-                                    parameter_combinations)
+  # Check if it's intervention
+  if (!is.null(xreg)) {
+    parameter_combinations <- rbind(parameter_combinations, parameter_combinations)
     half_length <- nrow(parameter_combinations) / 2
     intv_status <- matrix(c(rep("INTV", half_length), rep(" ", half_length)), ncol = 1)
   } else {
@@ -170,87 +183,88 @@ manual_arima <- function(data, p, d, q, s = NULL,
   bic_model <- matrix("-", nrow = nrow(parameter_combinations))
   
   for (i in 1:nrow(parameter_combinations)) {
+    params <- parameter_combinations[i, ]
+    cat("Processing combination:", params, "\n")
+    
     tryCatch({
-      params <- parameter_combinations[i, ]
-      
-      if (is.null(s)){
-        if (intv_status[i] == " " | is.null(xreg)) {
-          model <- Arima(data, order = params[1:3])
+      withCallingHandlers({
+        if (is.null(s)) {
+          if (intv_status[i] == " " | is.null(xreg)) {
+            model <- Arima(data, order = params[1:3])
+          } else {
+            model <- Arima(data, order = params[1:3], xreg = xreg)
+          }
         } else {
-          model <- Arima(data, order = params[1:3], xreg = xreg)
+          if (intv_status[i] == " " | is.null(xreg)) {
+            model <- Arima(data, order = params[1:3], seasonal = list(order=params[4:6], period=s))
+          } else {
+            model <- Arima(data, order = params[1:3], seasonal = list(order=params[4:6], period=s), xreg = xreg)
+          }
         }
-      } else
-        if (intv_status[i] == " " | is.null(xreg)) {
-          model <- Arima(data, order = params[1:3], 
-                         seasonal = list(order=params[4:6],period=s))
+        
+        aicc_model[i] <- round(model$aicc, 3)
+        bic_model[i] <- round(model$bic, 3)
+        aic_model[i] <- round(model$aic, 3)
+        
+        coeftesting <- lmtest::coeftest(model)
+        if (all(coeftesting[, 4] < 0.05)) {
+          coef_test[i] <- "PASS"
         } else {
-          model <- Arima(data, order = params[1:3], 
-                         seasonal = list(order=params[4:6],period=s), 
-                         xreg = xreg)
+          coef_test[i] <- "FAIL"
         }
-      
-      aicc_model[i] <- round(model$aicc, 3)
-      bic_model[i] <- round(model$bic, 3)
-      aic_model[i] <- round(model$aic, 3)
-      
-      coeftesting <- lmtest::coeftest(model)
-      if (all(coeftesting[,4] < 0.05)) {
-        coef_test[i] <- "PASS"
-      } else {
-        coef_test[i] <- "FAIL"
-      }
-      
-      invisible(capture.output(residualtesting <- checkresiduals(model, 
-                                                                 plot = FALSE)))
-      if (residualtesting$p.value > 0.05) {
-        ljung_test[i] <- paste0("PASS (p-value: ", 
-                                sprintf("%.3f", residualtesting$p.value), ")")
-      } else {
-        ljung_test[i] <- paste0("FAIL (p-value: ", 
-                                sprintf("%.3f", residualtesting$p.value), ")")
-      }
-      
-      residualnormality <- shapiro.test(residuals(model))
-      if (residualnormality$p.value > 0.05) {
-        normal_test[i] <- paste("PASS (p-value:", 
-                                sprintf("%.3f", residualnormality$p.value), ")")
-      } else {
-        normal_test[i] <- paste("FAIL (p-value:", 
-                                sprintf("%.3f", residualnormality$p.value), ")")
-      }
-      
-      status[i] <- "CLEAR"
+        
+        residualtesting <- checkresiduals(model, plot = FALSE)
+        if (residualtesting$p.value > 0.05) {
+          ljung_test[i] <- paste0("PASS (p-value: ", sprintf("%.3f", residualtesting$p.value), ")")
+        } else {
+          ljung_test[i] <- paste0("FAIL (p-value: ", sprintf("%.3f", residualtesting$p.value), ")")
+        }
+        
+        residualnormality <- shapiro.test(residuals(model))
+        if (residualnormality$p.value > 0.05) {
+          normal_test[i] <- paste("PASS (p-value:", sprintf("%.3f", residualnormality$p.value), ")")
+        } else {
+          normal_test[i] <- paste("FAIL (p-value:", sprintf("%.3f", residualnormality$p.value), ")")
+        }
+        
+        status[i] <- "CLEAR"
+      }, warning = function(war) {
+        cat("Warning for combination:", params, "\n")
+        cat("Warning message:", war$message, "\n")
+        invokeRestart("muffleWarning")
+      })
     }, error = function(err) {
+      cat("Error for combination:", params, "\n")
+      cat("Error message:", err$message, "\n")
     })
   }
   
   # Combine Parameter into 1 Column
   parameter_combinations <- cbind(parameter_combinations, intv_status)
   
-  if (is.null(s)){
+  if (is.null(s)) {
     parameter_combinations <- apply(parameter_combinations, 1, function(x) {
       paste0("(", x[1], ", ", x[2], ", ", x[3], ") ", x[4])
     })
   } else {
     parameter_combinations <- apply(parameter_combinations, 1, function(x) {
-      paste0("(", x[1], ", ", x[2], ", ", x[3], ")  (", x[4]
-             , ", ", x[5], ", ", x[6], ")  ", s, "  ", x[7])
+      paste0("(", x[1], ", ", x[2], ", ", x[3], ")  (", x[4], ", ", x[5], ", ", x[6], ")  ", s, "  ", x[7])
     })
   }
   
   # Make the Dataframe
   df <- data.frame(Model = parameter_combinations)
-  df$"Status" = as.vector(status)
-  df$"Coefficient Significance Test" = as.vector(coef_test)
-  df$"Ljung-Box Residual Test" = as.vector(ljung_test)
-  df$"Saphiro Wilk Residual Normality Test" = as.vector(normal_test)
-  df$"AIC Test" = as.vector(aic_model)
-  df$"AICC Test" = as.vector(aicc_model)
-  df$"BIC Test" = as.vector(bic_model)
+  df$"Status" <- as.vector(status)
+  df$"Coefficient Significance Test" <- as.vector(coef_test)
+  df$"Ljung-Box Residual Test" <- as.vector(ljung_test)
+  df$"Shapiro-Wilk Residual Normality Test" <- as.vector(normal_test)
+  df$"AIC Test" <- as.vector(aic_model)
+  df$"AICC Test" <- as.vector(aicc_model)
+  df$"BIC Test" <- as.vector(bic_model)
   
   arima_list <- list(df)
   names(arima_list) <- c("model_list")
-  comment(arima_list) <- "Basic Coefficient Test, Ljung-Box Residual Test, and Saphiro-Wilk Residual Normality Test for each possible tentative model"
+  comment(arima_list) <- "Basic Coefficient Test, Ljung-Box Residual Test, and Shapiro-Wilk Residual Normality Test for each possible tentative model"
   
   return(arima_list)
 }
@@ -286,14 +300,95 @@ model_selector <- function(arima_list){
 
 #-------------------------------------------------------------------------------
 
-# Transform Back the Transformed Data & Plot
-transform_back <- function(forecast_object, lambda){
-  n <- length(lambda)
+#Intervention Helper
+generate_intervention <- function(ts_data, intervention_point, types = c("step", "ramp", "pulse"), 
+                                  pulse_start = NULL, pulse_end = NULL, mode = c("regular", "forecast"), 
+                                  previous_data_last_number = NULL) {
+  # Determine the mode
+  mode <- match.arg(mode)
   
-  forecast_object$upper <- InvBoxCox(forecast_object$upper, lambda[1])
-  forecast_object$lower <- InvBoxCox(forecast_object$lower, lambda[1])
-  forecast_object$mean <- InvBoxCox(forecast_object$mean, lambda[1])
-  forecast_object$x <- InvBoxCox(forecast_object$x, lambda[1])
+  # Check if ts_data is a time series or a number (length of the array)
+  if (is.numeric(ts_data) && length(ts_data) == 1) {
+    time_index <- seq_len(ts_data)
+  } else {
+    time_index <- time(ts_data)
+  }
+  
+  # Create intervention variables based on the specified types
+  interventions <- list()
+  
+  if (mode == "regular") {
+    if ("step" %in% types) {
+      step_intervention <- ifelse(time_index >= intervention_point, 1, 0)
+      interventions$step_intervention <- step_intervention
+    }
+    
+    if ("ramp" %in% types) {
+      ramp_intervention <- pmax(0, time_index - intervention_point + 1)
+      interventions$ramp_intervention <- ramp_intervention
+    }
+    
+    if ("pulse" %in% types) {
+      if (is.null(pulse_start) | is.null(pulse_end)) {
+        stop("For pulse intervention, both pulse_start and pulse_end need to be specified.")
+      }
+      pulse_intervention <- ifelse(time_index >= pulse_start & time_index <= pulse_end, 1, 0)
+      interventions$pulse_intervention <- pulse_intervention
+    }
+  } else if (mode == "forecast") {
+    if ("step" %in% types) {
+      step_intervention <- rep(1, length(time_index))
+      interventions$step_intervention <- step_intervention
+    }
+    
+    if ("ramp" %in% types) {
+      if (is.null(previous_data_last_number)) {
+        stop("For ramp intervention in forecast mode, previous_data_last_number must be specified.")
+      }
+      last_ramp_value <- previous_data_last_number
+      ramp_intervention <- seq(last_ramp_value + 1, last_ramp_value + length(time_index))
+      interventions$ramp_intervention <- ramp_intervention
+    }
+    
+    if ("pulse" %in% types) {
+      pulse_intervention <- rep(0, length(time_index))
+      interventions$pulse_intervention <- pulse_intervention
+    }
+  }
+  
+  # Combine interventions into a data frame
+  intervention_df <- as.data.frame(interventions)
+  
+  return(intervention_df)
+}
+
+# Transform Back the Transformed Data & Plot
+transform_back <- function(forecast_object, lambda_scale_list) {
+  
+  # Extract the scale factor and lambda list
+  scale <- lambda_scale_list$scale
+  lambda_list <- lambda_scale_list$lambdas
+  
+  if (length(lambda_list) == 1) {
+    lambda <- lambda_list[1]
+    forecast_object$upper <- InvBoxCox(forecast_object$upper, lambda)
+    forecast_object$lower <- InvBoxCox(forecast_object$lower, lambda)
+    forecast_object$mean <- InvBoxCox(forecast_object$mean, lambda)
+    forecast_object$x <- InvBoxCox(forecast_object$x, lambda)
+  } else {
+    for (i in seq_along(lambda_list)) {
+      forecast_object$upper <- InvBoxCox(forecast_object$upper, lambda_list[i])
+      forecast_object$lower <- InvBoxCox(forecast_object$lower, lambda_list[i])
+      forecast_object$mean <- InvBoxCox(forecast_object$mean, lambda_list[i])
+      forecast_object$x <- InvBoxCox(forecast_object$x, lambda_list[i])
+    }
+  }
+  
+  # Rescale the forecast object
+  forecast_object$upper <- forecast_object$upper / scale
+  forecast_object$lower <- forecast_object$lower / scale
+  forecast_object$mean <- forecast_object$mean / scale
+  forecast_object$x <- forecast_object$x / scale
   
   return(forecast_object)
 }
